83cf250e4b16f7bc1f5a307d09be26cfe521b213
victor-arana <victor_arana@hotmail.com>
2020-08-10 14:24:26 -0500

    :construction: add get endpoint

--- src/main/java/no/data/cards/CardsController.java
+++ src/main/java/no/data/cards/CardsController.java
@@ -1,5 +1,6 @@
 package no.data.cards;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,10 @@ public class CardsController {
     @PostMapping
     public void createCard(){
         System.out.println("Creating card");
+    }
+
+    @GetMapping
+    public void readCards(){
+        System.out.println("Getting cards");
     }
 }
