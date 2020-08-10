415deb1ebe7258735cca9bb1e9c291d96abf3800
victor-arana <victor_arana@hotmail.com>
2020-08-10 14:02:42 -0500

    :tada: initialize project

---
+++ src/main/java/no/data/cards/DigitalCardsApplication.java
@@ -0,0 +1,13 @@
+package no.data.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/data/cards/DigitalCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package no.data.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
