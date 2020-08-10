6f32e0dc424666f9df460025b539ccbf7dcfad46
victor-arana <victor_arana@hotmail.com>
2020-08-10 14:15:52 -0500

    :construction: add post endpoint

---
+++ src/main/java/no/data/cards/CardsController.java
@@ -0,0 +1,15 @@
+package no.data.cards;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/card")
+public class CardsController {
+
+    @PostMapping
+    public void createCard(){
+        System.out.println("Creating card");
+    }
+}
