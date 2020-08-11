9dd0f8e01062173717e5b94281768da15bad63e1
victor-arana <victor_arana@hotmail.com>
2020-08-10 21:06:11 -0500

    :card_file_box: model, add static list

--- src/main/java/no/data/cards/controller/CardsController.java
+++ src/main/java/no/data/cards/controller/CardsController.java
@@ -1,10 +1,28 @@
 package no.data.cards.controller;
 
+import no.data.cards.model.Card;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/card")
 public class CardsController {
+
+    private static List<Card> cards = new ArrayList<>();
+
+    static{
+        Card card = new Card();
+        card.setId(1);
+        card.setTitularName("Victor Jose Arana Rodriguez");
+        card.setEntityName("Space Money");
+        card.setCardNumber("1234-1234-1234-1324");
+        card.setMark("Space Money");
+        card.setExpirationDate("10/12");
+
+        cards.add(card);
+    }
 
     @PostMapping
     public void createCard(){
